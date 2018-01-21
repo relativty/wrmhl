b14b1553d0acba090811b74ede37cdcfb606274c
Stellaris-code <Stellaris-code@users.noreply.github.com>
2018-01-21 08:54:11 +0100

    Update wrmhlRead.cs

--- Assets/WRMHL/Scripts/wrmhlRead.cs
+++ Assets/WRMHL/Scripts/wrmhlRead.cs
@@ -31,11 +31,11 @@ public class wrmhlRead : MonoBehaviour {
 	[Tooltip("Timeout")]
 	public int ReadTimeout = 20;
 
-	[Tooltip("QueueLenght")]
-	public int QueueLenght = 1;
+	[Tooltip("QueueLength")]
+	public int QueueLength = 1;
 
 	void Start () {
-		myDevice.set (portName, baudRate, ReadTimeout, QueueLenght); // This method set the communication with the following vars;
+		myDevice.set (portName, baudRate, ReadTimeout, QueueLength); // This method set the communication with the following vars;
 		//                              Serial Port, Baud Rates, Read Timeout and QueueLenght.
 		myDevice.connect (); // This method open the Serial communication with the vars previously given.
 	}
